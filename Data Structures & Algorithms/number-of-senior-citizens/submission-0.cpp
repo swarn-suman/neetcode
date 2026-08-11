08f73213b6661c4a3daf1866e6da8c448fd15018
Swarn Suman <swarnsuman004@gmail.com>
2026-08-11 22:16:10 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+    int result =0;
+
+    for(int i=0; i<details.size() ; i++)
+        {
+
+            if((details[i][11]-'0')*10+(details[i][12]-'0') > 60)
+             result++;
+        }
+    return result ;
+    }
+};
