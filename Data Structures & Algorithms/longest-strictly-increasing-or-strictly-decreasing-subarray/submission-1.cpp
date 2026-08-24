0f86cde251a4d6ed5d894d97f79872d344842695
Swarn Suman <swarnsuman004@gmail.com>
2026-08-24 21:50:59 +0530

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-1

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int inc = 1, dec = 1, res = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                inc = dec = 1;
+            } else if (nums[i] > nums[i - 1]) {
+                inc = inc + 1;
+                dec = 1;
+            } else {
+                inc = 1;
+                dec = dec + 1;
+            }
+            res = max(res, max(inc, dec));
+        }
+
+        return res;
+    }
+};
